133c8e6221325e5edb2168985eab5343daacfa9a
Wilfried-project <project.creation.christopher@gmail.com>
2026-07-20 22:10:26 +0000

    Fix Supabase keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://cyzfkwbjrcxwejzpioja.supabase.co'
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN5emZrd2JqcmN4d2VqenBpb2phIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4MTY5ODQsImV4cCI6MjA5ODM5Mjk4NH0.5KH-wpibqzcfk--Oe9EE9QMKKfc8h-DxYn9aH_i4_f0'
+const SUPABASE_URL = 'https://ehgxpsywmamgsgfrvhna.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoZ3hwc3l3bWFtZ3NnZnJ2aG5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM1Nzg5MzAsImV4cCI6MjA5OTE1NDkzMH0.ICad3rKNUACqFFn-r2NEGSdp4TaHgpEM2Dph--FfwPc'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
   auth: { persistSession: true, autoRefreshToken: true }
